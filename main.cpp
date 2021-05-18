ff6d9d51cdf221c9a6cd648dbe874aca374c0fa7
jose111777 <78769761+jose111777@users.noreply.github.com>
2021-05-18 09:29:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hola mundo!" << endl;
-    return 0;
+#include<iostream.h>
+#include<conio.h>
+void main(){
+    int n, cp=0,ci=0,c=0,x;
+    clrscr();
+    cout<<"Ingresar cantidad de numeros:";
+    cin>>n;
+    while (c<n){
+    cout<<"Ingresar numero:";
+    cin>>x;
+    if(x % 2==0)
+    cp=cp+1;
+    else
+    ci=ci+1;
+    c=c+1;
+}
+cout<<"los numeros pares son:"<<cp<<endl;
+cout<<"los numeros impares son:"<<ci<<endl;
+getch();
 }
