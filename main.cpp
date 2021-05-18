3b9e6e5dc186f180dc9858c4dacd9a551efb28db
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2021-05-18 09:19:00 -0600

    Update del main.cpp
    Se realizo el programa

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hola mundo!" << endl;
+    int numero;
+    cout <<"Bienvenido al programa" <<endl;
+    cout <<"Ingrese un numero:" <<endl;
+    cin >> numero;
+    if (numero/2=0)
+        cout <<"El numero es par" <<endl;
+    
+    else if cout <<"El numero es impar" <<endl;
+
+    cout <<"Nombre: Ruben Fernando Espinoza Fernandez" <<endl;
+    Cout << "Carne: 1525620" <<endl;
+
     return 0;
 }
