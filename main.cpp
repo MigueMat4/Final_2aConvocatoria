d2e1d84e0942ca354044d8148f13c6a359fafbcb
Krhistopher <krissiguenza@gmail.com>
2021-05-18 09:26:43 -0600

    Versión 1.0.2
    Programa terminado.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hola mundo!" << endl;
-    return 0;
+    int numero;
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+    if (numero % 2 == 0)
+    {
+        cout << "El numero es par " << endl;
+        for (int i = numero - 2; i > 0; i = i - 2)
+        {
+            if (i > 2) 
+            {
+                cout << i << ", ";
+            }
+            else
+            {
+                cout << i << "." << endl;
+            }  
+        }
+    }
+    else
+    {
+        string numeroString = to_string(numero);
+        string numeroAlrevez = "";
+        for (int i = numeroString.length() -1; i > -1; i --)
+        {
+            numeroAlrevez.append(numeroString.substr(i, 1));
+
+        }
+        cout << numeroAlrevez;
+    }
 }
